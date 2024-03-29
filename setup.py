d7e31eb66d206c2e4b699e71e04d2bc20a380a3f
david-istvan <goerenh@gmail.com>
2024-03-29 18:05:24 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='subjective-rl',
+    version='0.1',
+    author='Kyanna Dagenais, Istvan David',
+    url='https://www.python.org/sigs/distutils-sig/',
+    packages=['src'],
+    package_dir={'src':'../src'},
+    include_package_data=True,
+    install_requires=[],
+    license='GNU General Public License v3.0'
+)
