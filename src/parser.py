76cbb7bbd7994a01c1a8d5cc0aa5872176f36d36
david-istvan <goerenh@gmail.com>
2024-04-02 20:06:46 +0200

    New parser for the new model and opinion file

--- src/parser.py
+++ src/parser.py
@@ -1,15 +1,18 @@
 import re
-from .model import Hint, Configuration
+from model import Hint, Configuration, Cell
 
 class Parser():
 
     def parse(self, file):
         with open(file, 'r') as f:
+            n = f.readline()
             u = f.readline()
             hints = []
             for line in f:
-                line = line.replace(" ", "")[:-1]
-                [cellid, opinion] = re.split(r',', line)
-                hints.append(Hint(int(cellid), int(opinion), float(u)))
+                [cell, opinion] = re.split(r' ', line)
+                coordinates = re.search(r'[0-9]{1},[0-9]{1}', cell)
+                [y, x] = re.split(r',', coordinates.group(0))
+                cell = Cell(int(y), int(x))
+                hints.append(Hint(cell, int(opinion), float(u)))
                 
-        return Configuration(float(u), hints)
+        return Configuration(int(n), float(u), hints)
