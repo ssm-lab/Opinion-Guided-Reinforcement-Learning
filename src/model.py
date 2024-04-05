0daf72dab394a7fde63197e57fdd0c6b8d3a7645
david-istvan <goerenh@gmail.com>
2024-04-05 20:29:06 +0200

    Grid to the model

--- src/model.py
+++ src/model.py
@@ -1,3 +1,16 @@
+class Grid():
+    def __init__(self, size:int):
+        self.size = size
+        self.cells =  []
+        
+        for y in range(size):
+            for x in range(size):
+                cell = Cell(y, x, size)
+                self.cells.append(cell)
+    
+    def get_cell_by_coordinates(self, y:int, x:int):
+        return self.cells[y*self.size + x]
+
 """
 Represents a cell
 """
@@ -10,17 +23,23 @@ class Cell():
     def __str__(self):
         return '({}, {})'.format(self.y, self.x)
         
-    def getCellAbove(self):
-        return None if self.y = 0 else [self.y-1, self.x]
+    def get_cell_above(self):
+        return None if self.y == 0 else [self.y-1, self.x]
         
-    def getCellBelow(self):
-        return None if self.y = self.map_size-1 else [self.y+1, self.x]
+    def get_cell_below(self):
+        return None if self.y == self.map_size-1 else [self.y+1, self.x]
     
-    def getCellToTheLeft(self):
-        return None if self.x = 0 else [self.y, self.x-1]
+    def get_cell_to_the_left(self):
+        return None if self.x == 0 else [self.y, self.x-1]
         
-    def getCellToTheRight(self):
-        return None if self.x = self.map_size-1 else [self.y, self.x+1]
+    def get_cell_to_the_right(self):
+        return None if self.x == self.map_size-1 else [self.y, self.x+1]
+        
+    def get_neighbors(self):
+        return [self.get_cell_to_the_left(), self.get_cell_below(), self.get_cell_to_the_right(), self.get_cell_above()]
+        
+    def get_sequence_number(self):
+        return self.y*self.map_size + self.x
 
 """
 Represents an opinion about a cell
@@ -60,3 +79,23 @@ class HumanInput():
         
     def __str__(self):
         return f'Human input with {len(self.hints)} hints at uncertainty level {self.u}.'
+        
+        
+cell00 = Cell(0, 0, 4)
+cell01 = Cell(0, 1, 4)
+#print(cell00)
+#print(cell01)
+
+grid = Grid(4)
+for cell in grid.cells:
+    print(cell)
+    
+print(grid.get_cell_by_coordinates(0, 2))   # should print 0,2
+print(grid.get_cell_by_coordinates(3, 3))   # should print 3,3
+print(grid.get_cell_by_coordinates(3, 3).get_sequence_number())   # should print 15
+
+print(grid.get_cell_by_coordinates(3, 3).get_cell_above())  # should print 2,3
+print(grid.get_cell_by_coordinates(3, 3).get_cell_below())  # should print None
+print(grid.get_cell_by_coordinates(3, 3).get_cell_to_the_left())  # should print 3,2
+print(grid.get_cell_by_coordinates(3, 3).get_cell_to_the_right())  # should print None
+print(grid.get_cell_by_coordinates(3, 3).get_neighbors())  # should print (3,2), None, None, (2,3)
