db62d43782d76429ef315c362e02fdba67f8586c
david-istvan <goerenh@gmail.com>
2024-04-05 20:05:29 +0200

    Followed up changes in the parser too

--- src/parser.py
+++ src/parser.py
@@ -1,18 +1,18 @@
 import re
-from model import Hint, Configuration, Cell
+from model import Hint, HumanInput, Cell
 
 class Parser():
 
     def parse(self, file):
         with open(file, 'r') as f:
-            n = f.readline()
+            map_size = f.readline()
             u = f.readline()
             hints = []
             for line in f:
                 [cell, opinion] = re.split(r' ', line)
                 coordinates = re.search(r'[0-9]{1},[0-9]{1}', cell)
                 [y, x] = re.split(r',', coordinates.group(0))
-                cell = Cell(int(y), int(x), int(n))
+                cell = Cell(int(y), int(x), int(map_size))
                 hints.append(Hint(cell, int(opinion), float(u)))
                 
-        return Configuration(int(n), float(u), hints)
+        return HumanInput(int(map_size), float(u), hints)
