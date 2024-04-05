069a132bfe1077658a2b34aaf6e4ff4dc72c8525
david-istvan <goerenh@gmail.com>
2024-04-05 20:04:31 +0200

    Updated model

--- src/model.py
+++ src/model.py
@@ -2,25 +2,25 @@
 Represents a cell
 """
 class Cell():
-    def __init__(self, y:int, x:int, n:int):
+    def __init__(self, y:int, x:int, map_size:int):
         self.y = y
         self.x = x
-        self.n = n
+        self.map_size = map_size
         
     def __str__(self):
         return '({}, {})'.format(self.y, self.x)
         
     def getCellAbove(self):
-        return Cell(max(self.y-1, 0), self.x)
+        return None if self.y = 0 else [self.y-1, self.x]
         
     def getCellBelow(self):
-        return Cell(min(self.y+1, self.n-1), self.x)
+        return None if self.y = self.map_size-1 else [self.y+1, self.x]
     
-    def getCellToTheRight(self):
-        return Cell(self.y, max(self.x-1, 0))
-        
     def getCellToTheLeft(self):
-        return Cell(self.y, min(self.x+1, self.n-1))
+        return None if self.x = 0 else [self.y, self.x-1]
+        
+    def getCellToTheRight(self):
+        return None if self.x = self.map_size-1 else [self.y, self.x+1]
 
 """
 Represents an opinion about a cell
@@ -37,19 +37,26 @@ class Hint():
         return 'Hint(cell: {}, hint: {}). (At uncertainty = {}. =>belief = {}, =>disbelief = {}.)'.format(self.cell, self.opinion, self.u, self.b, self.d)
         
     def normalizeBeliefForUncertainty(self):
-        #these are hard-coded values to be replaced when we generalize the framework... famous last words
+        #these are hard-coded values to be replaced when we generalize the framework...
         self.b = (self.opinion + 2) * ((1 - self.u)/(4))
         self.d = 1 - (self.b + self.u)
+        
+        # some floating-point issues, as usual with Python
+        self.b = round(self.b, 4)
+        self.d = round(self.b, 4)
         
     def get_P(self):
         return b + a*u
 
 """
-Full configuration with every Hint and the uncertainty parameter
+Human input: a collection of hints and the uncertainty parameter
 """
-class Configuration():
+class HumanInput():
     
-    def __init__(self, n: float, u: float, hints):
-        self.n = n
+    def __init__(self, map_size: int, u: float, hints):
+        self.map_size = map_size
         self.u = u
         self.hints = hints
+        
+    def __str__(self):
+        return f'Human input with {len(self.hints)} hints at uncertainty level {self.u}.'
