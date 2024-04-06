b888c0e264e21d1415986de8f5f925ef86d7a173
david-istvan <goerenh@gmail.com>
2024-04-06 16:54:52 +0200

    Adding new test module

---
+++ tests/model_tests.py
@@ -0,0 +1,23 @@
+import unittest
+from src.model import Hint, Cell
+import os
+
+class ModelTests(unittest.TestCase):
+    
+    def testNormalization(self):
+        opinion = 1
+        u = 0.2
+        expectedBelief = 0.6
+        expectedDisbelief = 0.2
+        
+        cell = Cell(0, 0, 0)
+        
+        hint = Hint(cell, opinion, u)
+        
+        self.assertEqual(hint.opinion, opinion)
+        
+        self.assertAlmostEqual(hint.b, expectedBelief, delta=0.0001)
+        self.assertAlmostEqual(hint.d, expectedDisbelief, delta=0.0001)
+        
+if __name__ == "__main__":
+    unittest.main()
