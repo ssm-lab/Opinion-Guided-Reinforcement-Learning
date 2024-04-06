999b8a1882b6b161e4c69081e025fcea85efd1a3
david-istvan <goerenh@gmail.com>
2024-04-06 20:18:24 +0200

    Removed junk

--- src/model.py
+++ src/model.py
@@ -106,36 +106,3 @@ class HumanInput():
         
     def __str__(self):
         return f'Human input with {len(self.hints)} hints at uncertainty level {self.u}.'
-        
-'''
-
-cell00 = Cell(0, 0, 4)
-cell01 = Cell(0, 1, 4)
-#print(cell00)
-#print(cell01)
-'''
-'''
-grid = Grid(4)
-
-for cell in grid.cells:
-    print(cell)
-
-print(grid.get_cell_by_coordinates(0, 2))   # should print 0,2
-print(grid.get_cell_by_coordinates(3, 3))   # should print 3,3
-print(grid.get_cell_by_coordinates(3, 3).get_sequence_number_in_grid())   # should print 15
-
-print(grid.get_cell_by_coordinates(3, 3).get_cell_in_direction(Direction.UP))  # should print 2,3
-print(grid.get_cell_by_coordinates(3, 3).get_cell_in_direction(Direction.DOWN))  # should print None
-print(grid.get_cell_by_coordinates(3, 3).get_cell_in_direction(Direction.LEFT))  # should print 3,2
-print(grid.get_cell_by_coordinates(3, 3).get_cell_in_direction(Direction.RIGHT))  # should print None
-print(grid.get_cell_by_coordinates(3, 3).get_neighbors())  # should print (3,2), None, None, (2,3)
-
-print(grid.cells[15].get_action_to_me_from_neighbor(Direction.RIGHT))
-print(grid.cells[15].get_action_to_me_from_neighbor(Direction.RIGHT).value)
-
-
-print(f'me: {grid.cells[5]}')
-print(grid.cells[5].get_actions_to_me_from_all_neighbors())
-print(f'me: {grid.cells[0]}')
-print(grid.cells[0].get_actions_to_me_from_all_neighbors())
-'''
--- src/sl.py
+++ src/sl.py
@@ -24,37 +24,4 @@ def beliefConstraintFusion(opinion1, opinion2):
     a = (a1 * (1 - u1) + a2 * (1 - u2)) / (2 - u1 - u2)
     
     
-    return [b, d, u, a]    
-    
-'''
-
-cell00 = Cell(0, 0, 4)
-cell01 = Cell(0, 1, 4)
-#print(cell00)
-#print(cell01)
-'''
-'''
-grid = Grid(4)
-
-for cell in grid.cells:
-    print(cell)
-
-print(grid.get_cell_by_coordinates(0, 2))   # should print 0,2
-print(grid.get_cell_by_coordinates(3, 3))   # should print 3,3
-print(grid.get_cell_by_coordinates(3, 3).get_sequence_number_in_grid())   # should print 15
-
-print(grid.get_cell_by_coordinates(3, 3).get_cell_in_direction(Direction.UP))  # should print 2,3
-print(grid.get_cell_by_coordinates(3, 3).get_cell_in_direction(Direction.DOWN))  # should print None
-print(grid.get_cell_by_coordinates(3, 3).get_cell_in_direction(Direction.LEFT))  # should print 3,2
-print(grid.get_cell_by_coordinates(3, 3).get_cell_in_direction(Direction.RIGHT))  # should print None
-print(grid.get_cell_by_coordinates(3, 3).get_neighbors())  # should print (3,2), None, None, (2,3)
-
-print(grid.cells[15].get_action_to_me_from_neighbor(Direction.RIGHT))
-print(grid.cells[15].get_action_to_me_from_neighbor(Direction.RIGHT).value)
-
-
-print(f'me: {grid.cells[5]}')
-print(grid.cells[5].get_actions_to_me_from_all_neighbors())
-print(f'me: {grid.cells[0]}')
-print(grid.cells[0].get_actions_to_me_from_all_neighbors())
-'''
+    return [b, d, u, a]
