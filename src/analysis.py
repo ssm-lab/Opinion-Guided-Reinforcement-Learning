f950d6ee8af2a0a3a1f7089f6b7419f52b70f40a
david-istvan <goerenh@gmail.com>
2024-05-01 16:23:40 -0400

    Heatmap generation

--- src/analysis.py
+++ src/analysis.py
@@ -1,74 +1,143 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
+import numpy as np
+from enum import Enum
+from map_tools import MapTools
 
-episodes = 10000
+episodes = 5000
 size = 12
 seed = 63
 filename = f'{size}x{size}-seed{seed}'
-inputFolder = './experiments/human5'
+inputFolder = './experiments/02-pilot-full/human5'
 outputFolder = './analysis-output'
+experiments_input_path = './input'
 
-def loadData():
-    df_random = pd.read_csv(f'{inputFolder}/{episodes}/reward_data/random/{filename}.csv', header=None)
-    df_no_advice = pd.read_csv(f'{inputFolder}/{episodes}/reward_data/noadvice/{filename}.csv', header=None)
-    df_advice_00 = pd.read_csv(f'{inputFolder}/{episodes}/reward_data/advice/{filename}-u-0.01.csv', header=None)
-    df_advice_02 = pd.read_csv(f'{inputFolder}/{episodes}/reward_data/advice/{filename}-u-0.2.csv', header=None)
-    df_advice_04 = pd.read_csv(f'{inputFolder}/{episodes}/reward_data/advice/{filename}-u-0.4.csv', header=None)
-    df_advice_06 = pd.read_csv(f'{inputFolder}/{episodes}/reward_data/advice/{filename}-u-0.6.csv', header=None)
-    df_advice_08 = pd.read_csv(f'{inputFolder}/{episodes}/reward_data/advice/{filename}-u-0.8.csv', header=None)
-    df_advice_10 = pd.read_csv(f'{inputFolder}/{episodes}/reward_data/advice/{filename}-u-1.0.csv', header=None)
-    return df_random, df_no_advice, df_advice_00, df_advice_02, df_advice_04, df_advice_06, df_advice_08, df_advice_10
+class DataKind(Enum):
+    REWARD = 'reward'
+    POLICY = 'policy'
+
+
+def loadData(data_kind):
+    df_random = pd.read_csv(f'{inputFolder}/{episodes}/{data_kind.value}_data/random/{filename}.csv', header=None)
+    df_no_advice = pd.read_csv(f'{inputFolder}/{episodes}/{data_kind.value}_data/noadvice/{filename}.csv', header=None)
+    df_advice_00 = pd.read_csv(f'{inputFolder}/{episodes}/{data_kind.value}_data/advice/{filename}-u-0.01.csv', header=None)
+    df_advice_02 = pd.read_csv(f'{inputFolder}/{episodes}/{data_kind.value}_data/advice/{filename}-u-0.2.csv', header=None)
+    df_advice_04 = pd.read_csv(f'{inputFolder}/{episodes}/{data_kind.value}_data/advice/{filename}-u-0.4.csv', header=None)
+    df_advice_06 = pd.read_csv(f'{inputFolder}/{episodes}/{data_kind.value}_data/advice/{filename}-u-0.6.csv', header=None)
+    df_advice_08 = pd.read_csv(f'{inputFolder}/{episodes}/{data_kind.value}_data/advice/{filename}-u-0.8.csv', header=None)
+    df_advice_10 = pd.read_csv(f'{inputFolder}/{episodes}/{data_kind.value}_data/advice/{filename}-u-1.0.csv', header=None)
+    
+    return {
+        'random': df_random,
+        'no_advice': df_no_advice,
+        'advice_00': df_advice_00,
+        'advice_02': df_advice_02,
+        'advice_04': df_advice_04,
+        'advice_06': df_advice_06,
+        'advice_08': df_advice_08,
+        'advice_10': df_advice_10
+    }
     
     
 def cumulative_reward():
-    dfs = loadData()
-    
+    dfs = loadData(DataKind.REWARD)
     
     fig = plt.figure()
     ax = plt.gca()
     
-    dfs_names = ['random', 'no advice', 'advice-0.0', 'advice-0.2', 'advice-0.4', 'advice-0.6', 'advice-0.8', 'advice-1.0']
-    
-    for idx, df in enumerate(dfs):
-        ax = df.mean().plot(label=dfs_names[idx])
+    for df_name, df in dfs.items():
+        mean = df.mean()
+        std = df.std()
+        x = np.arange(len(mean))
+        plt.plot(x, mean, label=df_name)
+        plt.fill_between(x, mean - std, mean + std, alpha=0.2)
         
-    plt.yscale('log')
+    #plt.yscale('log')
     plt.xlabel('Episode')
     plt.ylabel('Cumulative Reward')
     plt.legend()
     plt.show()
+
+def heatmap():
+    dfs = loadData(DataKind.POLICY)
     
+    #print(dfs['advice_00'])
+    df = dfs['advice_00'].mean().to_frame()
     
-    """
-    years = df['Publication year'].value_counts().sort_index().to_frame().reset_index()
-    years.columns = ['year', 'papers']
-
-    #print(years)
-
-    bins = [2007, 2009, 2014, 2019, 2024]
-    labels = ['-2009', '2010-2014', '2015-2019', '2020-2024']
-
-    years['yearbins'] = pd.cut(years['year'], bins, labels=labels)
-
-    #print(years)
-
-    #print(years.groupby('yearbins').sum()['papers'])
-
-    fig = plt.figure(figsize = (10, 5))
+    jss = []
+    for i in range(0, 144):
+        jss.append([i, i, i, i])
+    cellids = [j for js in jss for j in js]
     
-    ax = years.groupby('yearbins').sum()['papers'].plot(kind='bar', rot=0, color='#43b82c')
+    df.insert(0, 'cellid', cellids)
+    df.columns.values[1] = 'prob'
     
-    ax.set_ylabel('Papers', fontsize=15)
-    ax.set_xlabel('Years', fontsize=15)
-    ax.bar_label(ax.containers[0], fontsize=12)
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
+    dss = []
+    for d in range(0, 144):
+        dss.append(['←', '↓', '→', '↑'])
+        #dss.append(['L', 'D', 'R', 'U'])
+    directions = [d for ds in dss for d in ds]
     
-    plt.savefig(f'{outputFolder}/papers-per-year.pdf', format='pdf', bbox_inches='tight')
+    df['direction'] = directions
+    
+    print(df)
+    
+    df = df.sort_values('prob', ascending=False).drop_duplicates(['cellid'])
+    df = df.sort_values('cellid', ascending=True).reset_index(drop=True)
+    #df = df.drop(['cellid'], axis=1)
+    
+    size = 12
+    seed = 63
+    
+    df = df.assign(row = lambda x: (x['cellid'] // 12))
+    df = df.assign(col = lambda x: (x['cellid'] % 12))
+    
+    map_description = MapTools(experiments_input_path).parse_map(size, seed)
+    print(map_description)
+    
+    terminals = []
+    for rid, row in enumerate(map_description):
+        for cell in range(0, len(row)):
+            #print(row[cell])
+            if row[cell] in ('H', 'G'):
+                terminals.append(size*rid+cell)
+                
+    for t in terminals:
+        df.loc[t, 'prob'] = 0.0
+        df.loc[t, 'direction'] = ''
+        
+    df = df.loc[df['prob'] !=0.25]
+    df = df.loc[df['prob'] !=0.0]
+    
+    print(df)
+    
+    result = df.pivot(index='row', columns='col', values='prob')
+    print(result)
+    
+    directions = df.pivot(index='row', columns='col', values='direction')
+    print(directions)
+
+    sns.heatmap(
+        result,
+        linewidths=0.001,
+        linecolor='gray',
+        annot=directions,
+        fmt='',
+        cmap=sns.color_palette("Blues", as_cmap=True),
+        vmin=0.0,
+        vmax=1.0,
+        xticklabels=[],
+        yticklabels=[],
+        annot_kws={"fontsize": "x-large"}
+    )
     plt.show()
-    """
     
-    
+
+#cumulative_reward()
+heatmap()
+
+
 """
     def plot_success_rate(self, no_advice_success_rates, advice_success_rates, human_input):
         logging.getLogger().setLevel(logging.INFO)
@@ -105,27 +174,7 @@ def cumulative_reward():
         
         plt.savefig(filename, format='pdf', bbox_inches='tight')
         #plt.show()
-        
-    def plot_cumulative_rewards(self, all_cumulative_rewards, human_input):
-        for i in range(self._NUM_EXPERIMENTS):
-            start = i * self._MAX_EPISODES
-            end = (i + 1) * self._MAX_EPISODES
-            df = all_cumulative_rewards.iloc[start:end]
-
-            plt.figure()
-            plt.plot(df.index.values, df['noadvice'], label = 'No advice')
-            plt.plot(df.index.values, df['advice'], label = 'Advice')
-            plt.title(f'Map: {self._MAP_NAME}; eps={str(self._MAX_EPISODES)}; exps={str(self._NUM_EXPERIMENTS)}; u={round(human_input.u, 4)}.')
-            plt.xlabel('Episode')
-            plt.ylabel('Cumulative Reward')
-            plt.legend()
-
-        filename = f'{self.get_file_name(extension="pdf", advice_explicit=False, u_explicit=True, human_input=human_input, extra="CUMULATIVEREWARD")}'
-        plt.savefig(filename, format='pdf', bbox_inches='tight')
-
-        plt.show()
 """
     
     
-    
-cumulative_reward()
+
