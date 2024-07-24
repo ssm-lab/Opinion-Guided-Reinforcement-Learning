c85138e2ac68a4bd41f0a1ce6dba3083c13bb0cb
Kyanna Dagenais <dagenaik@mcmaster.ca>
2024-07-24 15:28:07 -0400

    Updated advice_parser.py and advice_parser_tests.py

--- src/advice_parser.py
+++ src/advice_parser.py
@@ -2,17 +2,19 @@ import re
 from model import Cell, Advice, AdvisorInput
 
 
-class AdviceParser():
+class AdviceParser:
 
-    def parse(self, file):
+    @staticmethod
+    def parse(file):
         with open(file, 'r') as f:
             map_size = f.readline()
             advice_list = []
             for line in f:
                 [cell, value] = re.split(r' ', line)
                 coordinates = re.search(r'[0-9]+,[0-9]+', cell)
+                assert (len(cell) - 3) == len(coordinates.group())
                 [y, x] = re.split(r',', coordinates.group(0))
                 cell = Cell(int(y), int(x), int(map_size))
                 advice_list.append(Advice(cell, int(value)))
-                
+
         return AdvisorInput(int(map_size), advice_list)
--- tests/advice_parser_tests.py
+++ tests/advice_parser_tests.py
@@ -1,8 +1,24 @@
 import pytest
+import os
+from src.advice_parser import AdviceParser
 
 
-def test():
-    assert True
+@pytest.fixture()
+def advice_parser():
+    yield AdviceParser()
+
+
+def test_valid_parser_input(advice_parser):
+    file = os.path.abspath("valid_parser_input.txt")
+    with open(file, 'r') as f:
+        lines = len(f.readlines())
+        expected_num_advice = lines - 1
+
+    advisor_input = advice_parser.parse(file)
+
+    assert advisor_input.map_size >= 0
+    assert expected_num_advice == len(advisor_input.advice_list)
+
 
 if __name__ == "main":
     pytest.main()
